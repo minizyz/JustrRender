15c22a63ca0e4f50c2863dd17495eaa545e994bc
minizyz <163286241+minizyz@users.noreply.github.com>
2026-08-31 21:39:25 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 // Top-level build file for JustrRender
 plugins {
-    id("com.android.application") version "8.9.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
+    id("com.android.application") version "9.1.1" apply false
+    id("com.android.library") version "9.1.1" apply false
+    kotlin("jvm") version "2.0.21" apply false
 }
+
