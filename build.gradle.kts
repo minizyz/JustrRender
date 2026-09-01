8df23d97fbb9af24ab665a086ebc7ed8d0fcc154
minizyz <163286241+minizyz@users.noreply.github.com>
2026-09-01 18:51:34 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.9.1" apply false
-    id("com.android.library") version "8.9.1" apply false
-    kotlin("jvm") version "1.9.25" apply false
+    id("com.android.application") version "9.1.1" apply false
+    id("com.android.library") version "9.1.1" apply false
+    kotlin("jvm") version "2.0.21" apply false
 }
