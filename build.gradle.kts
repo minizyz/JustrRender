78f46f5fb6ce078beef12a0158f066bc7010b284
minizyz <163286241+minizyz@users.noreply.github.com>
2026-09-01 19:00:05 +0800

    fix: 修复Kotlin插件冲突，kotlin(jvm)改为org.jetbrains.kotlin.android

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
     id("com.android.application") version "9.1.1" apply false
     id("com.android.library") version "9.1.1" apply false
-    kotlin("jvm") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
