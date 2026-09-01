c0f2af9a39906db2fcc78bcc923fb4e59aa3d3c3
minizyz <163286241+minizyz@users.noreply.github.com>
2026-09-01 19:33:20 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,8 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
+        resValues = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.15"
